d56cab4c9c6cda4a8a9b50ca8bc707c532f98ca4
3A <524019351@qq.com>
2019-04-13 12:41:58 +0800

    搞定求值结构

--- node.h
+++ node.h
@@ -1,21 +1,50 @@
 #pragma once
 #include "gate.h"
 
+class line
+{
+private:
+    node *n;
+    unsigned int sub;
+public:
+    line(node *n,unsigned int sub) : n(n), sub(sub) {}
+    bool get()
+    {
+        if(!n->isEval)
+            n->eval();
+        return n->result[sub];
+    }
+};
+
 class node
 {
-    public:
-    vector<node*> outputNode; //与g的output个数相等。每个连接需指明插槽位
+private:
+    blist getInputPar()
+    {
+        blist par;
+        for(line* l : inputNode)
+            par.push_back(l->get());
+        return par;
+    }
+
+public:
+    bool isEval=false;
+    vector<line*> inputNode; //与g的input个数相等。每个连接需指明插槽位
+    blist result;
     gate *g;
+
+    node(gate* g) : g(g) {}
 
     ~node()
     {
         delete g;
-        for(unsigned int i=0;i<allpar.size();i++)
-            delete allpar[i];
+        for(unsigned int i=0;i<inputNode.size();i++)
+            delete inputNode[i];
     }
 
-    blist calu(blist input)
+    blist eval()
     {
-        blist result=g->calu(input);
+        this->result=g->calu(getInputPar());
+        this->isEval=true;
     }
 }
